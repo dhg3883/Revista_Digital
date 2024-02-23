0429f552d570dfbffeef4d57910b14a3ae88f2e3
David Hernández <126130418+dhg3883@users.noreply.github.com>
2024-02-23 15:59:56 -0600

    Initial commit

---
+++ docs/JS/index.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {  
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showw')
+        }else{
+            entry.target.classList.remove('showw')
+        }
+    });
+})
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((el) => observer.observe(el))
+if (screen.width<=800){
+    $("td.r").replaceWith("<tr>",$("td.r"),"</tr>")
+}
